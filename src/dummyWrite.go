9f46b2eccbaa9f34d9132ea0df32f104735e1fa6
Hung Nguyen <hung135@hotmail.com>
2016-09-27 21:37:30 -0400

    inital

--- src/dummyWrite.go
+++ src/dummyWrite.go
@@ -5,12 +5,18 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	//"io/ioutil"
 	"os"
 	// "math/rand"
 
 	// "io/ioutil"
+	"encoding/binary"
+	//"fmt"
+	"math/rand"
+	"io/ioutil"
+	"fmt"
+	"net/http"
+	"io"
 )
 
 func check(e error) {
@@ -18,68 +24,81 @@ func check(e error) {
 		panic(e)
 	}
 }
+
 type RefSet struct {
 	pdna [] byte
 }
 
-
-func main() {
+func MakeDummyfile(filepath string, totalSize int) {
 
 	// To start, here's how to dump a string (or just
 	// bytes) into a file.
-
-	p:= [][]int8{}
+	//const totalCount  = 1000000
+	p := [][]byte{}
 	//p :=[100]RefSet{}
-	ff, err2 := os.Create("c:\\temp\\pdnadat2")
+	ff, err2 := os.Create(filepath)
 	check(err2)
 	x := bufio.NewWriter(ff)
-	for jj := 0; jj < 1000000; jj++ {
-		var ranp []int8
+	for jj := 0; jj < totalSize; jj++ {
+		var ranp []byte
 		for ii := 0; ii < 144; ii++ {
-			ranp= append(ranp,int8(ii))
+			ranp = append(ranp, byte(rand.Int()))
 			//fmt.Printf("aa")
 
 		}
 		//fmt.Printf("\n")
-		p=append(p,ranp)
+		p = append(p, ranp)
 
-		fmt.Fprint(x, ranp,"\n")
+		binary.Write(x, binary.BigEndian, ranp)
+		//fmt.Print( ranp)
 	}
-
 	x.Flush() // Don't forget to flush!
 
-	/*
+}
 
-*/
-	// For more granular writes, open a file for writing.
-	f, err := os.Create("c:\\temp\\dat2")
+func Downloadfile(url string, filename string) {
+
+	out, err := os.Create(filename)
+	check(err)
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	_, err = io.Copy(out, resp.Body)
 	check(err)
 
-	// It's idiomatic to defer a `Close` immediately
-	// after opening a file.
-	defer f.Close()
+}
+func ComparePDNA(dat []byte,reportDNA []byte)  int {
 
-	// You can `Write` byte slices as you'd expect.
-	d2 := []byte{115, 111, 109, 101, 10}
-	n2, err := f.Write(d2)
+	for i := 1; i <= 100; i++ {
+		
+		for j := 1; j <= 144; j++ {
+			fmt.Print(dat[(i * 144)-j])
+
+		}
+		fmt.Println("")
+		//ComparePDNA(refDNA,reportDNA)
+	}
+
+
+	return 0
+}
+func main() {
+
+		MakeDummyfile("c:\\temp\\pdnadata100.bin",100)
+		MakeDummyfile("c:\\temp\\reportpdnadata1000.bin",1000)
+		MakeDummyfile("c:\\temp\\pdnadata10000.bin",10000)
+		MakeDummyfile("c:\\temp\\pdnadata100000.bin",100000)
+
+
+	Downloadfile("https://upload.wikimedia.org/wikipedia/commons/d/db/Patern_test.jpg", "c:\\temp\\filefromgoogle2.jpg")
+
+	dat, err := ioutil.ReadFile("c:\\temp\\pdnadata100.bin")
 	check(err)
-	fmt.Printf("wrote %d bytes\n", n2)
+	reportDNA, err := ioutil.ReadFile("c:\\temp\\reportpdnadata1000.bin")
+	check(err)
 
-	// A `WriteString` is also available.
-	n3, err := f.WriteString("writes\n")
-	fmt.Printf("wrote %d bytes\n", n3)
-
-	// Issue a `Sync` to flush writes to stable storage.
-	f.Sync()
-
-	// `bufio` provides buffered writers in addition
-	// to the buffered readers we saw earlier.
-	w := bufio.NewWriter(f)
-	n4, err := w.WriteString("buffered\n")
-	fmt.Printf("wrote %d bytes\n", n4)
-
-	// Use `Flush` to ensure all buffered operations have
-	// been applied to the underlying writer.
-	w.Flush()
-
+	//fmt.Print(string(dat))
+	//fmt.Print(len(dat))
+	ComparePDNA(dat, reportDNA)
 }
